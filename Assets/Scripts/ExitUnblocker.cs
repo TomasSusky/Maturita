6c60ffd3cc84f8b2ffbe2ce660a40faaa7062ef1
Tomáš Suský <suskytomas0@gmail.com>
2026-03-16 00:44:32 +0100

    Added new location - coin house and final platforms

--- Assets/Scripts/ExitUnblocker.cs
+++ Assets/Scripts/ExitUnblocker.cs
@@ -9,17 +9,17 @@ public class ExitUnblocker : MonoBehaviour
     public void UnblockExit()
     {
         var gs = FindFirstObjectByType<GameSession>();
-        if (gs != null && gs.CheckIfHasEnoughPoints(5000))
+        if (gs != null && gs.CheckIfHasEnoughPoints(100))
         {
             levelExit.GetComponent<BoxCollider2D>().isTrigger = true;
-            gs.SubstractScore(5000);
+            gs.SubstractScore(100);
         }
     }
 
     public void CheckPoints()
     {
         var gs = FindFirstObjectByType<GameSession>();
-        if (gs != null && gs.CheckIfHasEnoughPoints(5000))
+        if (gs != null && gs.CheckIfHasEnoughPoints(100))
         {
             dialogueActivator.UpdateDialogueObject(sufficientPointsDialogue);
         }
